86cc9a3d7528de052264bab1a8a683bce00086f7
vgoicoecheaca <vgoicoecheaca@gmail.com>
2022-08-06 19:45:44 +0000

    Changes to swf converter

--- contribs/simulator/swf2trace.c
+++ contribs/simulator/swf2trace.c
@@ -23,7 +23,7 @@ typedef struct job_trace {
     char username[MAX_USERNAME_LEN];
     long int submit; /* relative or absolute? */
     int  duration;
-    int  wclimit;
+    int wclimit;
     int  tasks;
     char qosname[MAX_QOSNAME];
     char partition[MAX_QOSNAME];
@@ -44,12 +44,13 @@ int main(int argc, char* argv[])
 {
     int i;
     int idx=0, errs=0;
-    int nrecs=10000; // Number of records has to be entered here each time. This should be done differently.
+    int nrecs=2583275; // Number of records has to be entered here each time. This should be done differently.
     job_trace_t* job_trace,* job_trace_head,* job_arr,* job_ptr;
     char const* const fileName = argv[1]; /* should check that argc > 1 */
     FILE* file = fopen(fileName, "r"); /* should check the result */
     char line[256];
     char *p;
+    const char* partitions[] = {"shared","gpu","srun","sandbox","exclusive","shared-long"};
 
     job_arr = (job_trace_t*)malloc(sizeof(job_trace_t)*nrecs);
     if (!job_arr) {
@@ -59,22 +60,23 @@ int main(int argc, char* argv[])
     job_trace_head = &job_arr[0];
     job_ptr = &job_arr[0];
     while (fgets(line, sizeof(line), file)) {
+	if (nrecs%idx==0) {printf("%d / %d  \n",idx,nrecs);} 
         /* note that fgets don't strip the terminating \n, checking its
            presence would allow to handle lines longer that sizeof(line) */
-        printf("%s", line);
+        //printf("%s", line);
         p = strtok(line, " ");
         i=0;
         while(p!=NULL){
-            if(i==0) { printf("%s", p); job_arr[idx].job_id = atoi(p); }   
-            if(i==1) { printf("%s", p); job_arr[idx].submit = 100 + atoi(p); }  // why submit cannot start from 0? 
-            if(i==3) { printf("%s", p); job_arr[idx].duration = atoi(p); }   
-            if(i==7) { printf("%s", p); job_arr[idx].tasks = ceil((float) (atoi(p)/CPUS_PER_NODE)); }
-            //if(i==7) { printf("%s", p); job_arr[idx].tasks = ceil((float) atoi(p)); }
-            if(i==8) { printf("%s", p); job_arr[idx].wclimit = atoi(p); }   
+            if(i==0) { job_arr[idx].job_id = atoi(p); }   
+            if(i==1) {  job_arr[idx].submit = 100 + atoi(p); }  // why submit cannot start from 0? 
+            if(i==3) {  job_arr[idx].duration = atoi(p); }   
+            //if(i==7) { printf("%s", p); job_arr[idx].tasks = ceil((float) (atoi(p)/CPUS_PER_NODE)); }
+            if(i==7) {  job_arr[idx].cpus_per_task = ceil((float) atoi(p)); }
+            if(i==8) {  job_arr[idx].wclimit = atoi(p) ; }   
             //if(i==11) { printf("%s", p); strcpy(job_arr[idx].username, p); }   
             //if(i==12) { printf("%s", p); strcpy(job_arr[idx].account, p); }   
-            //if(i==15) { printf("%s", p); strcpy(job_arr[idx].partition, p); }   
-            printf(" %s\n", p);
+            if(i==15) {  strcpy(job_arr[idx].partition, partitions[atoi(p)-1]); }   
+            //printf(" %s\n", p);
             p = strtok(NULL," ");
             i++; 
         }
@@ -84,12 +86,13 @@ int main(int argc, char* argv[])
         //else job_arr[idx].tasks_per_node = CPUS_PER_NODE;
 
         //assuming one task per node, and as many threads as CPUs per node
-        job_arr[idx].cpus_per_task = CPUS_PER_NODE;
+        //job_arr[idx].cpus_per_task = CPUS_PER_NODE;
         job_arr[idx].tasks_per_node = 1;
-
+	job_arr[idx].tasks = 1;
+        
         // for now keep username, partition and account constant.
         strcpy(job_arr[idx].username, "tester");
-        strcpy(job_arr[idx].partition, "normal");
+        //strcpy(job_arr[idx].partition, "normal");
         strcpy(job_arr[idx].account, "1000");
         //strcpy(job_trace->manifest_filename, "|\0");
         //job_trace->manifest=NULL; 
